90e4de29fffc31fc6faff74c57ba71b1b411d108
Owlpetcrypto <owlpetcrypto@gmail.com>
2023-02-17 23:07:18 +0100

    'Update FAQ revert placement of mint'

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Box, Flex, Link, Image } from '@chakra-ui/react'
 import TwitterLogo from './assets/twitter.svg'
 import DiscordLogo from './assets/discord.svg'
-import PortalLogo from './assets/portal.jpeg'
 import Mint from './mint'
 
 function BorderedBox({ title, subtitle, children }) {
@@ -36,27 +35,24 @@ const Main = ({ accounts, setAccounts }) => {
         </Box>
       </Flex>
 
+      <Mint />
+
       <Flex padding="0px 12px" flexDirection="column">
         <div className="snugpassbox">
           <div>
+            <p>The future of Web3 with our game-changing project:</p>
             <p>
-              A Web3 infrastructure ecosystem, a technology based project,
-              implementing NFT tech (digital ownership verficiation) in Web2
-              company systems
+              A Web3 infrastructure ecosystem that uses NFT tech to verify
+              digital ownership in Web2 company systems, an easy-to-use product
+              for building services without <em>BIG</em> developers.
             </p>
             <br></br>
             <p>
-              Launching an easy-to-use product for building services without BIG
-              developers
-            </p>
-            <p>
-              Snug Pass growth will become increasingly likely for web2
-              companies to make the transition.
+              Snug Pass will pave the way for Web2 companies to make the
+              transition
             </p>
           </div>
         </div>
-
-        <Mint />
 
         <div>
           <hr color="#00ffff" width="80%" marginTop="5rem"></hr>
@@ -70,37 +66,42 @@ const Main = ({ accounts, setAccounts }) => {
 
         <div className="grid">
           <BorderedBox subtitle="Phase 1">
-            <p>- Launch 2000 collection passes with Lifetime access</p>
-            <p>- Highlight importance of NFT Tech</p>
-            <p>- Reveal in-depth plans with sneak peeks of product testings</p>
+            <p>&#9670; Launch 2000 collection passes with Lifetime access</p>
+            <p>&#9670; Highlight importance of NFT Tech</p>
+            <p>
+              &#9670; An exclusive look at our in-depth plans with sneak peeks
+              of product testing
+            </p>
           </BorderedBox>
 
           <BorderedBox subtitle="Phase 2">
             <p>
-              - Launch 5 main products: Web3 shopify, Connect-to-Wallet
+              &#9670; Launch 5 main products: Web3 shopify, Connect-to-Wallet
               software, Web2.5 payment gateways, Smart escrow and Custom
               Contract Generator
             </p>
             <p>
-              - Utilise mint funds to expand the development of the products.
-              Additionally, public beta testers can enjoy a complimentary 7-day
+              &#9670; Utilise mint funds to expand the development of the
+              products. Additionally, public beta testers can enjoy a FREE 7-day
               trial.
             </p>
           </BorderedBox>
 
           <BorderedBox subtitle="Phase 3">
             <p>
-              - Apply monthly subscription for non-holders; The money raised is
-              reinvested in developing the product ecosystem and expanding the
-              brand
+              &#9670; Apply monthly subscription for non-holders; The money
+              raised is reinvested in developing the product ecosystem and
+              expanding the brand
             </p>
-            <p>- Schedule IRL events to promote SnugPass NFT innovations</p>
+            <p>
+              &#9670; Schedule IRL events to promote SnugPass NFT innovations
+            </p>
           </BorderedBox>
 
           <BorderedBox subtitle="ECOSYSTEM">
             <p>
-              - NFT Tech: Our products provide equivalent services to the web2
-              version, but they rely entirely on blockchain technology,
+              &#9670; NFT Tech: Our products provide equivalent services to the
+              web2 version, but they rely entirely on blockchain technology,
               utilizing dynamic smart contracts, tokenization, and tracking
               systems. This enhances decentralization, transparency, and
               security
@@ -124,35 +125,41 @@ const Main = ({ accounts, setAccounts }) => {
         </h1>
       </Box>
       <div className="faq-section">
-        <BorderedBox>
-          <div>
-            <p>1. What is the total supply of Snug Pass?</p>
-            <p className="answers">- 2000 lifetime passes</p>
-            <p>2. What is the mint price?</p>
-            <p className="answers">
-              - Snug List: 0.1 ETH | Public (any remains): 0.1 ETH
-            </p>
-            <p>3. How many Snug Passes can I mint?</p>
-            <p className="answers">- Up to 2 passes</p>
-            <p>4. What utilities does Snug Pass provide?</p>
-            <p className="answers">
-              - By obtaining a Snug Pass, you will gain exclusive, lifetime
-              access to our products and technologies ahead of everyone else.
-              Rather than paying a recurring monthly fee, this pass allows you
-              to join our collaborative and exclusive community to contribute to
-              our ecosystem. As a result, you'll become one of the pioneers of
-              Snug Pass technology.
-            </p>
-            <p>5. What is the benefit of holding 2 passes?</p>
-            <p className="answers">
-              - While having just one pass grants you full access to the Snug
-              ecosystem, holding two passes offers additional benefits,
-              including future rewards and perks. These may include owning a
-              share in the company and receiving a percentage of its profits.
-              Essentially, having two passes is superior to having just one.
-            </p>
-          </div>
-        </BorderedBox>
+        <details>
+          <summary>&#9670; What is the total supply of Snug Pass?</summary>
+          <p className="answers">&#9671; 2000 lifetime passes</p>
+        </details>
+        <details>
+          <summary>
+            &#9670; What is the Snug List and Public mint price?
+          </summary>
+          <p className="answers">&#9671; 0.1 ETH</p>
+        </details>
+        <details>
+          <summary>&#9670; How many Snug Passes can I mint?</summary>
+          <p className="answers">&#9671; Up to 2 passes</p>
+        </details>
+        <details>
+          <summary>&#9670; What utilities does Snug Pass provide?</summary>
+          <p className="answers">
+            &#9671; By obtaining a Snug Pass, you will gain exclusive, lifetime
+            access to our products and technologies ahead of everyone else.
+            Rather than paying a recurring monthly fee, this pass allows you to
+            join our collaborative and exclusive community to contribute to our
+            ecosystem. As a result, you'll become one of the pioneers of Snug
+            Pass technology.
+          </p>
+        </details>
+        <details>
+          <summary>&#9670; What is the benefit of holding 2 passes?</summary>
+          <p className="answers">
+            &#9671; While having one pass grants you full access to the Snug
+            ecosystem, holding two passes offers additional benefits, including
+            future rewards and perks. These may include owning a share in the
+            company and receiving a percentage of its profits. Essentially,
+            having two passes is superior to having one.
+          </p>
+        </details>
       </div>
 
       <div>
