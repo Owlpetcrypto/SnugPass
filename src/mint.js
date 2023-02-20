32a042c0c315f2b88c490616bff417215b176ed0
Digi | Toxic <DigiToxic55@gmail.com>
2023-02-19 19:05:42 -0500

    Digi

--- src/mint.js
+++ src/mint.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { Flex, Button } from '@chakra-ui/react'
-import { ethers } from 'ethers'
-import { keccak256 } from 'ethers/lib/utils'
-import MerkleTree from 'merkletreejs'
-import abi from './abi/abi.json'
+import React, { useState, useEffect } from "react";
+import { Flex, Button } from "@chakra-ui/react";
+import { ethers } from "ethers";
+import { keccak256 } from "ethers/lib/utils";
+import MerkleTree from "merkletreejs";
+import abi from "./abi/abi.json";
 
 const addresses = [
-
   "0x2894Df8C3C0D654674108613f2fcf0cF4E319b65",
   "0x675704E137D1178854bfa798fbee24069e081395",
   "0x903fC0d00Ab75B25C2F876D1f4242d86d0A19565",
@@ -24,198 +23,188 @@ const addresses = [
 const contractAddress = "0xEc3013634b69928a4daB8bBbe82a3c218e84eEf7";
 const API_KEY = ""; // Your api key from your ethercan account
 
-
 function Mint() {
-  const [mintAmount, setMintAmount] = useState(1)
-  const [currentAccount, setCurrentAccount] = useState(null)
-  let [totalSupply, setSupply] = useState('')
+  const [mintAmount, setMintAmount] = useState(1);
+  const [renderCount, setRenderCount] = useState(0);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [showFirst, setShowFirst] = useState(true);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
-  const [proof, setProof] = useState([''])
+  let [totalSupply, setSupply] = useState("");
 
-  let leaf = ''
+  const [proof, setProof] = useState([""]);
+
+  let leaf = "";
+
+  const handleClick = () => {
+    setShowFirst(!showFirst);
+    setButtonClicked(true);
+  };
+
+  const handleButtonClick = async () => {
+    await connectWalletHandler();
+    setTimeout(async () => {
+      await mintNftHandler();
+    }, 1000); // wait for 1 second before calling the second function
+  };
+  
+  
 
   const checkWalletIsConnected = async () => {
-    const { ethereum } = window
+    const { ethereum } = window;
 
     if (!ethereum) {
-      console.log('Make sure you have Metamask installed!')
-      return
+      console.log("Make sure you have Metamask installed!");
+      return;
     } else {
-      console.log('Wallet exist! We are ready to go!')
-      const accounts = await ethereum.request({ method: 'eth_accounts' })
+      console.log("Wallet exist! We are ready to go!");
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-      const leaves = addresses.map((x) => keccak256(x))
-      const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
-      const buf2hex = (x) => '0x' + x.toString('hex')
+      const leaves = addresses.map((x) => keccak256(x));
+      const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+      const buf2hex = (x) => "0x" + x.toString("hex");
 
-      console.log('root', buf2hex(tree.getRoot()))
+      console.log("root", buf2hex(tree.getRoot()));
 
-      leaf = keccak256(accounts[0]) // accounts from accounts using accountsconnect/metamask
-      setProof(tree.getProof(leaf).map((x) => buf2hex(x.data)))
+      leaf = keccak256(accounts[0]); // accounts from accounts using accountsconnect/metamask
+      setProof(tree.getProof(leaf).map((x) => buf2hex(x.data)));
 
-      console.log('proof', proof)
-      console.log(accounts)
+      console.log("proof", proof);
+      console.log(accounts);
 
-      if (accounts.lenght !== 0) {
-        const account = accounts[0]
-        console.log('Found an authorized account: ', account)
-        setCurrentAccount(account)
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
       } else {
-        console.log('No authorized account found')
+        console.log("No authorized account found");
       }
     }
-  }
+  };
+
   const connectWalletHandler = async () => {
-    const { ethereum } = window
+    const { ethereum } = window;
 
     if (!ethereum) {
-      alert('Please install Metamask!')
+      alert("Please install Metamask!");
     }
 
     try {
       const accounts = await ethereum.request({
-        method: 'eth_requestAccounts',
-      })
-      console.log('Found an account! Address: ', accounts[0])
-      setCurrentAccount(accounts[0])
+        method: "eth_requestAccounts",
+      });
+      console.log("Found an account! Address: ", accounts[0]);
+      setWalletConnected(true);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const mintNftHandler = async () => {
     try {
-      const { ethereum } = window
+      const { ethereum } = window;
 
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const nftContract = new ethers.Contract(contractAddress, abi, signer)
-        let amount = mintAmount
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+        let amount = mintAmount;
 
-        console.log('Initialize payment')
-        let cost = 0.1 * amount
+        console.log("Initialize payment");
+        let cost = 0.05 * amount;
         // let nftTxn = await nftContract.mint(amount, {value: ethers.utils.parseEther(cost.toString()),});
-        console.log('proof', proof)
+        console.log("proof", proof);
 
-        let nftTxn = await nftContract.presaleMint(amount, proof, {
-          value: ethers.utils.parseEther(cost.toString()),
-          
-        });
+        let nftTxn = await nftContract.presaleMint(amount, proof, {value: ethers.utils.parseEther(cost.toString()),});
         console.log("Minting... please wait!");
         await nftTxn.wait();
 
-
         console.log(
-          `Minted, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`,
-        )
+          `Minted, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        );
       } else {
-        console.log("Ethereum object doesn't exist")
+        console.log("Ethereum object doesn't exist");
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-
-  const connectWalletButton = () => {
-    return (
-      <p className="mint-yout-pass-button" onClick={connectWalletHandler}>
-        MINT YOUR PASS
-      </p>
-    )
-  }
-
-  const mintNftButton = () => {
-    return (
-      <p className="mint-yout-pass-button" onClick={mintNftHandler}>
-        MINT YOUR PASS
-      </p>
-    )
-  }
+  };
 
   useEffect(() => {
-    checkWalletIsConnected()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    if (renderCount < 1 && walletConnected) {
+      setRenderCount(renderCount + 1);
+      checkWalletIsConnected();
+    }
+  }, [walletConnected, renderCount]);
 
   async function fetchData() {
     const response = await fetch(
-      `https://api-goerli.etherscan.io/api?module=stats&action=tokensupply&contractaddress=${contractAddress}&apikey=${API_KEY}`,
-    )
-    const mintedTokens = await response.json()
-    setSupply(mintedTokens.result)
+      `https://api-goerli.etherscan.io/api?module=stats&action=tokensupply&contractaddress=${contractAddress}&apikey=${API_KEY}`
+    );
+    const mintedTokens = await response.json();
+    setSupply(mintedTokens.result);
   }
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   function increment() {
     //setCount(prevCount => prevCount+=1);
     setMintAmount(function (prevCount) {
-
       if (prevCount < 3) {
         return (prevCount += 1);
       } else {
         return (prevCount = 3);
-
       }
-    })
+    });
   }
 
   function decrement() {
     setMintAmount(function (prevCount) {
-
       if (prevCount === 1) {
         return (prevCount = 1);
       } else {
         return prevCount - 1;
-
       }
-    })
+    });
   }
 
   return (
     <div className="hover">
-      <Flex className="mint-text">
-
-        {currentAccount ?  <p>Mint is not live {/* <p>Snug List is Live!</p> <p>Public is Live!</p> */}</p> : ""}
-        
-      </Flex>
-      {currentAccount ? (
-        <div className="Button-mint">
-          <Button className="connect-button1" onClick={decrement}>
-            <div>-</div>
-          </Button>
-          <div>{mintAmount}</div>
-          <Button className="connect-button2" onClick={increment}>
-            <div>+</div>
-          </Button>
+      {!buttonClicked && (<div className="mint-yout-pass-button" onClick={handleClick}>MINT YOUR PASS</div>)}
+        {showFirst ? (
+          ""
+        ) : (
+          <div>
+          <Flex className="mint-text">
+            <p>Snug List is Live!</p>
+          </Flex>
+            <div className="Button-mint">
+              <Button className="connect-button1" onClick={decrement}>
+                <div>-</div>
+              </Button>
+              <div>{mintAmount}</div>
+              <Button className="connect-button2" onClick={increment}>
+                <div>+</div>
+              </Button>
+            </div>
+          <Flex>
+            <div className="mint-yout-pass-button" onClick={handleButtonClick}>MINT YOUR PASS</div>
+          </Flex>
+            <Flex className="mint-supply">
+              <div>
+                MINT PRICE: <span className="supply">0.1 ETH</span>
+              </div>
+            </Flex>
+            <Flex className="mint-supply">
+              <div>
+                Total Minted: <span className="supply">{totalSupply} / 2000</span>
+              </div>
+            </Flex>
         </div>
-      ) : (
-        ''
-      )}
-      <Flex>{currentAccount ? mintNftButton() : connectWalletButton()}</Flex>
-      {currentAccount ? (
-        <Flex className="mint-supply">
-          <div>
-            MINT PRICE: <span className="supply">0.1 ETH</span>
-          </div>
-        </Flex>
-      ) : (
-        ''
-      )}
-      {currentAccount ? (
-        <Flex className="mint-supply">
-          <div>
-            Total Minted: <span className="supply">{totalSupply} / 2000</span>
-          </div>
-        </Flex>
-      ) : (
-        ''
-      )}
-    </div>
-  )
+        )}
+      </div>
+  );
 }
 
-export default Mint
+export default Mint;
