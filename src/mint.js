495358111c5a8e2545d9714f9c8e01b5e7dda2db
Digi | Toxic <DigiToxic55@gmail.com>
2023-02-20 15:20:20 -0500

    Digi

--- src/mint.js
+++ src/mint.js
@@ -1,24 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Flex, Button } from "@chakra-ui/react";
 import { ethers } from "ethers";
 import { keccak256 } from "ethers/lib/utils";
 import MerkleTree from "merkletreejs";
 import abi from "./abi/abi.json";
-
-const addresses = [
-  "0x2894Df8C3C0D654674108613f2fcf0cF4E319b65",
-  "0x675704E137D1178854bfa798fbee24069e081395",
-  "0x903fC0d00Ab75B25C2F876D1f4242d86d0A19565",
-  "0x1E664bA846015F105FC97105f43E3803E0cA9A4c",
-  "0x9300614F448a4FD4512bA5586E41528019bf266D",
-  "0x2BbC590F901984b0217229Ab7F06Cd17d979fFb3",
-  "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
-  "0xE63f044D451934a08ced2463cBBf145e23ef6384",
-  "0xB9277bb50FA9bD8Da38085622f5B782D47a64339",
-  "0x9A41426ae9C88110188276bAF8046dC420005249",
-  "0xD028780dDCBBD7930a9C27da65D3ca6f440E5127",
-  "0x0AD4189a533d747AdbDA9afC8108c5B239dD73B4",
-]; // whitelisted address
+import WalletChecker from "./walletChecker";
+import { addresses } from "./addresses";
 
 const contractAddress = "0xEc3013634b69928a4daB8bBbe82a3c218e84eEf7";
 const API_KEY = ""; // Your api key from your ethercan account
@@ -45,7 +31,7 @@ function Mint() {
   const handleButtonClick = async () => {
     await connectWalletHandler();
   };
-  
+
   const checkWalletIsConnected = async () => {
     const { ethereum } = window;
 
@@ -107,24 +93,27 @@ function Mint() {
         let amount = mintAmount;
 
         console.log("Initialize payment");
-        let cost = 0.05 * amount;
-        let nftTxn = await nftContract.mint(amount, {value: ethers.utils.parseEther(cost.toString()),});
+        let cost = 0.049 * amount;
+        let nftTxn = await nftContract.mint(amount, {
+          value: ethers.utils.parseEther(cost.toString()),
+        });
         console.log("proof", proof);
 
         if (isProofSet) {
           // let nftTxn = await nftContract.presaleMint(amount, proof, {value: ethers.utils.parseEther(cost.toString()),});
           console.log("Minting... please wait!");
           await nftTxn.wait();
-    
+
           console.log(
             `Minted, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
           );
         } else {
           console.log("Proof has not been set yet.");
-        }};
-      } catch (err) {
-        console.log(err);
+        }
       }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -175,7 +164,9 @@ function Mint() {
 
   return (
     <div className="hover">
-      {!buttonClicked && (<div className="mint-yout-pass-button" onClick={handleClick}>MINT YOUR PASS</div>)}
+      <WalletChecker />
+
+      {/* {!buttonClicked && (<div className="mint-yout-pass-button" onClick={handleClick}>MINT YOUR PASS</div>)}
         {showFirst ? (
           <div className="first-display"></div>
         ) : (
@@ -206,8 +197,8 @@ function Mint() {
               </div>
             </Flex>
         </div>
-        )}
-      </div>
+        )} */}
+    </div>
   );
 }
 
---
+++ src/walletChecker.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { addresses } from "./addresses";
+
+function WalletChecker() {
+  const [address, setAddress] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleAddressChange(event) {
+    setAddress(event.target.value);
+  }
+
+  function handleCheckAddress() {
+    const isInArray = addresses.includes(address);
+
+    if (isInArray) {
+      setResult("The address is whitelisted");
+    } else {
+      setResult("The address is not whitelisted");
+    }
+  }
+
+  return (
+    <div className="flex">
+      <div>
+        <p className="text">{result}</p>
+      </div>
+      <div className="wallet-check">
+        <input
+          className="insite-wallet-check"
+          type="text"
+          id="address"
+          value={address}
+          placeholder="Write wallet here..."
+          onChange={handleAddressChange}
+        />
+      </div>
+      <div className="button-check">
+        <button
+          type="button"
+          className="insite-button-check"
+          onClick={handleCheckAddress}
+        >
+          Check address
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default WalletChecker;
